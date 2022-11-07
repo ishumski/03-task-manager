41fc243279e5e08672610f7da6047b838688ebdf
i.white <whalterwhite23@gmail.com>
2022-11-07 14:48:47 +0300

    add connection with mongodb

--- starter/app.js
+++ starter/app.js
@@ -1,22 +1,31 @@
 const express = require('express')
 const tasksRouter = require('./routes/tasks')
-require('./db/connection')
+const connectDB = require('./db/connection')
+require('dotenv').config()
 
 const app = express()
 const PORT = 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(PORT, () => {
+            console.log(`Server starts on port ${PORT}...`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+start()
 
 //middleware
 app.use(express.static('public'))
 app.use(express.json())
 
 app.use('/api/v1/tasks', tasksRouter)
-
-app.get('/hello', (req, res)=>{
+app.get('/hello', (req, res) => {
     res.send('Task manager app')
 })
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server starts on port ${PORT}...`)
-})
